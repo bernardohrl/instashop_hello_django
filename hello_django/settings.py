cba0f6109cad8203d16eb54c2f84191ebb4b5b72
Bernardo Henrique <bernardohrl.unb@gmail.com>
2018-07-30 23:19:53 -0300

    Removes unused packages

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello',
-    'material',
-    'material.frontend',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
