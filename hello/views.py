b8e97eeebd970439f1ed3bb365463f088072e265
Bernardo Henrique <bernardohrl.unb@gmail.com>
2018-07-31 00:44:55 -0300

    Does Name Form

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from .forms import NameForm
+
 def index(request):
-    context = {}
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            context = {'form': form, 'name': form['name'].value()}
+
+    else:
+        form = NameForm()
+        context = {'form': form}
+
     return render(request, 'index.html', context)
--- hello_django/settings.py
+++ hello_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*nj2wibsyg%gbj6&q^^yafb9t)t=y2x!90maf*8#t&1_899jm+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bernardohrl.pythonanywhere.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['bernardohrl.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
